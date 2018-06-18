8369c93c9d0f12af9ba9585a6802aa15d5db2d29
Shaecel-bortoletto <38111269+Shaecel-bortoletto@users.noreply.github.com>
2018-06-18 16:54:58 -0300

    Add files via upload

---
+++ Agenda.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		String[][] agenda = { { "Bill Gates", "1111-1111", "bill@outlook.com" },
+				{ "linus Torvalds", "2222-2222", "linus@gmail.com" },
+				{ "Steve Jobs", "3333-3333", "steve@icloud.com" } };
+		System.out.println(agenda[1][2]);
+
+	}
+
+}
