2b68d05e1c8fa8de3143ecf3160e4c83aafd6c37
Shaecel-bortoletto <38111269+Shaecel-bortoletto@users.noreply.github.com>
2018-06-18 16:22:08 -0300

    Add files via upload

---
+++ ArrayMultidimensional.java
@@ -0,0 +1,16 @@
+package br.com.java;
+
+public class ArrayMultidimensional {
+
+	public static void main(String[] args) {
+		// alinha abaixo cria um array multidimensional(matriz)
+		double[][] boletim = { { 8,7,9,3} ,{ 4,5,8,6} };
+		// recuperando a nota de portugues do 3o bimestre
+		System.out.println(" Média de Português do 3o bime:" + boletim[1][2]);
+		
+		
+		}
+
+	}
+
+
